e68681b824c78d796c66cc61fa3a91d5241a359e
Natalie Marotzek <natalie.jane1215>
2019-12-11 12:46:26 +1100

    Update 11/12/2019
    Added new sprite sheets and edited script

--- Script.js
+++ Script.js
@@ -1,19 +1,20 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-//create player object
+//create player object (remember to use American spelling)
+//this a test sprite until the actual player sprite is created
 var player = {
-    "pos_x": 250,
-    "pos_y": 250,
+    "pos_x": (c.height/2),
+    "pos_y": (c.width/2),
     "color": "cyan",
-    "unit_size": 5,
+    "unit_size": 15,
     "draw": function(){
         ctx.fillStyle = this.color;
         ctx.fillRect(this["pos_x"], this["pos_y"], this.unit_size, this.unit_size);
     },
     "update": function() {
         //we need to make sure the sprite can't leave the screen, or at least be returned to where it exited from
-        //this formula will work for any sized canvas, so copy it!
+        //this formula will work for any sized canvas, so copy it into your player script under the 'Draw' function!
         //checking up
         if (player.pos_y < 0) {
             player.pos_y = 0;
@@ -33,49 +34,86 @@ var player = {
     },
 };
 
+//set up a class for background sprites
+class backgroundSprite {
+  "pos_x" = 0;
+  "pos_y" = 0;
+  "width" = 20;
+  "height" = 20;
+  "image" = "#";
+  "draw" = function(){
+      var pat = ctx.createPattern(this.image, "repeat");
+      ctx.fillStyle = pat;
+      ctx.fill();
+      ctx.fillRect(this.pos_x, this.pos_y, this.width, this.height);
+  }
+};
+
+//make an array of backgroundSprites
+var bgSprites = [];
+
+//make a function for adding background sprites
+function addBGSprite(imagepath = '',pos_x = 0,pos_y = 0,width = 20, height = 20) {
+  var bgs = new backgroundSprite();
+  bgs.pos_x = pos_x;
+  bgs.pos_y = pos_y;
+  bgs.width = width;
+  bgs.height = height;bgs.image = document.getElementById(imagepath);
+  bgSprites.push(bgs);
+}
+
+addBGSprite("Cave", 0, 0, c.height, c.width);
+
+
+console.log(bgSprites);
 //set interval for game update
 setInterval(updateGameState, 1000 / 25);
 
-//this clear the previous frame when you move the sprite, preventing afterimage
+//this clears the previous frame when you move the sprite, preventing afterimage
 function updateGameState() {
     ctx.clearRect(0, 0, c.width, c.height);
+    //draw background
+    for (var i = 0; i < bgSprites.length; i++) {
+      bgSprites[i].draw();
+    }
+    //do player
     player.update();
     player.draw();
 };
 
 //set up event detection for pressing a key on the keyboard
+//this was made on the Google Chrome, just so it is known.  I don't know if it effects the numerical value of the keys at all
 document.addEventListener("keypress", function(event) {
 console.log(event.keyCode);
 
 //movement keys
-//pressing G = return to middle
-if (event.keyCode == 103) {
-  //alert("this returns you to middle");
-  player.pos_x = (c.width/2);
-  player.pos_y = (c.height/2);
+//pressing G = action button
+if (event.keyCode == 103 && true) {
+  //this is where you put the code to pick up the item
+  //picks up what you are standing in front of
 }
 
 //pressing W = move up
 if (event.keyCode == 119) {
-  //alert("you pressed 'up'");
+  //insert sprite frames here!
   player.pos_y -= player.unit_size;
 }
 
 //pressing S = move down
 if (event.keyCode == 115) {
-  //alert("you pressed 'down'");
+  //insert sprite frames here!
   player.pos_y += player.unit_size;
 }
 
 //pressing A = move left    
 if (event.keyCode == 97) {
-  //alert("you pressed 'left'");
+  //insert sprite frames here!
   player.pos_x -= player.unit_size;
 }
 
 //pressing D = move right
 if (event.keyCode == 100) {
-  //alert("you pressed 'right'");
+  //insert sprite frames here!
   player.pos_x += player.unit_size;
 }
 });
